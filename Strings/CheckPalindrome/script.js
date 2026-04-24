682a309f8f025c1ae9be641674e8e52dec5b320d
Pulkit Acharya <pullkit.9@gmail.com>
2026-04-24 13:28:40 +0530

    Added check palindrome

---
+++ Strings/CheckPalindrome/script.js
@@ -0,0 +1,12 @@
+let str = "Kumar";
+
+function checkPalindrome(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(checkPalindrome(str));
