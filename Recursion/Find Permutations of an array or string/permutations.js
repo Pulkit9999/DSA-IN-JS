59edd3ae4ee133f3541e1e296d30c5ccc5dff9d7
Pulkit Acharya <pullkit.9@gmail.com>
2026-09-11 08:28:44 +0530

    added correct code

--- Recursion/Find Permutations of an array or string/permutations.js
+++ Recursion/Find Permutations of an array or string/permutations.js
@@ -10,6 +10,8 @@ function recurPermute(arr, ds, freqMap, ans) {
       ds.push(arr[i]);
       freqMap.set(arr[i], 1);
       recurPermute(arr, ds, freqMap, ans);
+      freqMap.delete(arr[i]);
+      ds.pop();
     }
   }
 }
